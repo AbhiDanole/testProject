c7eba7ba5b2b84b3536009a5898b81ecaff2019e
Akash <Akash@192.168.0.104>
2019-12-12 00:29:36 +0530

    server port updated

--- demo/src/main/java/com/example/demo/controller/FirstController.java
+++ demo/src/main/java/com/example/demo/controller/FirstController.java
@@ -1,14 +1,14 @@
 package com.example.demo.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class FirstController {
 	
 	@RequestMapping("/")
 	public String display() {
-		return "index.jsp";
+		return "Hello World!!s";
 	}
 
 }
